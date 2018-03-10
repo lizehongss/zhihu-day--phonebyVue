0e5ab571604046a7aa0944ed44960c6b51874d1e
lizehongss <13600484164@163.com>
2018-03-10 16:24:39 +0800

    评论页样式完成

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
+#前言
+
+使用vue框架仿知乎日报移动端，本项目仅是出于学习目的。学习vue构建SPA页面。本项目包含5个单页，使用了自定义组件和自定义指令。
+
+#技术栈
+
+vue2+webpack+vue-router+axios+ES5/6+flex
+
 # 说明 
 - 图标库使用 [Iconfont-阿里巴巴矢量图标库](http://www.iconfont.cn/)
 - 移动布局采用flex布局
 - 使用自定义指令 **Chickoutside** 完成侧栏显示和隐藏
 - 使用[better-scroll](https://github.com/ustbhuangyi/better-scroll)实现上拉加载和下拉刷新
+- 使用自定义指令 **Time** 完成时间转换
 
 # API
--使用了[izzyleung](https://github.com/izzyleung/ZhihuDailyPurify/wiki/%E7%9F%A5%E4%B9%8E%E6%97%A5%E6%8A%A5-API-%E5%88%86%E6%9E%90)提供的文档
+- 使用了[izzyleung](https://github.com/izzyleung/ZhihuDailyPurify/wiki/%E7%9F%A5%E4%B9%8E%E6%97%A5%E6%8A%A5-API-%E5%88%86%E6%9E%90)提供的知乎日报-API-分析文档
+- 使用node的request做代理
 # 基本功能
 - [x] 首页导航 -- 完成
 - [x] 首页侧栏 -- 完成
-- [x] 文章详情页面 -- 完成
-- [x] 主题列表 -- 完成
+- [x] 文章详情页 -- 完成
+- [x] 主题列表页 -- 完成
+- [x] 评论页 -- 完成
+- [x] 主编信息页 -- 完成
+
 
 
 
--- views/commont.vue
+++ views/commont.vue
@@ -1,15 +1,29 @@
 <template>
 <div>
-	<div class="title">
+	<div class="title-f">
+		<div class="title">
+			<div class="left">
+				<svg class="icon" aria-hidden="true"
+				@click="handleback">
+					<use xlink:href="#icon-back"></use>
+				</svg>
+				<span>{{longComments.length+shortComments.length}} 条点评</span>
+			</div>
+		</div>
 	</div>
+	<div class="ch"></div>
 <div class="long-comments">
-	<div class="long-comments-lenghth"></div>
+	<div class="comments-lenghth">
+	{{longComments.length}} 条长评</div>
 	<div class="daily-comment" v-for="comment in longComments">
 		<div class="daily-comment-avator">
 			<img :src="comment.avatar">
 		</div>
 		<div class="daily-comment-content">
+			<div class="daily-comment-top">
 			<div class="daily-comment-name">{{comment.author}}</div>
+			<div class="daily-comment-like">赞 {{comment.likes}}</div>
+			</div>
 			<div class="daily-comment-time" v-Time="comment.time">
 			</div>
 			<div class="daily-comment-text">{{comment.content}}</div>
@@ -17,13 +31,18 @@
 	</div>
 </div>
 <div class="short-comments">
-	<div class="short-comments-lenghth"></div>
-	<div class="daily-comment" v-for="comment in shortComments">
+	<div class="comments-lenghth"
+	 @click="showshort=!showshort">
+	{{shortComments.length}} 条短评</div>
+	<div class="daily-comment" v-for="comment in shortComments" v-show="showshort">
 		<div class="daily-comment-avator">
 			<img :src="comment.avatar">
 		</div>
 		<div class="daily-comment-content">
+			<div class="daily-comment-top">
 			<div class="daily-comment-name">{{comment.author}}</div>
+			<div class="daily-comment-like">赞 {{comment.likes}}</div>
+			</div>
 			<div class="daily-comment-time" v-Time="comment.time">
 			</div>
 			<div class="daily-comment-text">{{comment.content}}</div>
@@ -43,7 +62,8 @@
 		data(){
 			return{
 				longComments: [],
-				shortComments: []
+				shortComments: [],
+				showshort: false
 			}
 		},
 		methods:{
@@ -64,6 +84,9 @@
 						return comment;
 					})
 				})
+			},
+			handleback(){
+				this.$router.go(-1);
 			}
 		},
 		mounted(){
@@ -74,3 +97,70 @@
 		},
 	}
 </script>
+<style scoped>
+	.daily-comment{
+	border-bottom: 1px dashed #e3e8ee;
+	display: flex;
+	justify-content: flex-start;
+	margin-top: 10px;
+	width: 100%;
+
+	}
+	.daily-comment-avator img{
+		border-radius: 50%;
+		width: 40px;
+		height: 40px;
+	}
+	.daily-comment-content{
+		margin-left: 1em;
+		width: 100%;
+	}
+	.daily-comment-time{
+		color:#9ea7b4;
+		font-size: 14px;
+		margin-top: 4px;
+	}
+	.daily-comment-text{
+		margin-top: 10px;
+	}
+	.daily-comment-top{
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+	}
+	.daily-comment-like{
+		margin-right: 1em;
+		font-size: 14px;
+	}
+	.comments-lenghth{
+		border-bottom: 1px dashed #e3e8ee;
+		font-size: 15px;
+		width: 100%;
+		margin-top:10px;
+	}
+	.title-f{
+		position: fixed;
+		top:0;
+		left: 0;
+		height:40px;
+		width: 100%;
+		z-index: 999;
+	}
+	.title{
+		background-color: #4348b7;
+		height: 40px;
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: #fff;
+	}
+	.icon{
+		margin-right: 1em;
+	}
+	.ch{
+		width: 100%;
+		height: 40px;
+		display: block;
+	}
+</style>
